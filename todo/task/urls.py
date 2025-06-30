3ca1e82735117c9e8f7adb8e1872597cf6138f0c
Mamunur1093 <mamunur1093@gmail.com>
2025-06-30 12:09:20 +0600

    Task Add, Edit, Delete, and View

--- todo/task/urls.py
+++ todo/task/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.task_index, name='task_index'),
+    path('delete/<int:task_id>/', views.task_delete, name='task_delete'),
+    path('edit/<int:task_id>/', views.task_edit, name='task_edit'),
+    path('update/<int:task_id>/', views.task_update, name='task_update'),
 ]
--- todo/task/views.py
+++ todo/task/views.py
@@ -1,6 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Task
+
 
 # Create your views here.
-
 def task_index(request):
-    return render(request, 'task/index.html')
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        task = Task.objects.create(title=title)
+        task.save()
+        return redirect('task_index')
+
+    tasks = Task.objects.all()
+    context = {'tasks': tasks}
+
+    return render(request, 'task/index.html', context)
+
+
+def task_delete(request, task_id):
+    task = Task.objects.get(id=task_id)
+    task.delete()
+    return redirect('task_index')
+
+
+def task_edit(request, task_id):
+    task = Task.objects.get(id=task_id)
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        task.title = title
+        task.save()
+        return redirect('task_index')
+
+
+    context = {'task': task}
+    return render(request, 'task/index.html', context)
+
+
+def task_update(request, task_id):
+    task = Task.objects.get(id=task_id)
+    task.is_complete = not task.is_complete
+    task.save()
+    return redirect('task_index')
